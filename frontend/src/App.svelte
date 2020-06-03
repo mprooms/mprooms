a926e365861e7a645a2ceda5a7ddef007acd53ef
Jens Schedel <jens.schedel@methodpark.de>
2020-06-03 07:35:26 +0200

    Add legend to bottom of file

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,6 +3,29 @@
 		width: 80%;
 		margin: 0 auto;
 	}
+
+	#legend td {
+		width: 2rem;
+		height: 2rem;
+	}
+
+	#legend td+td {
+		width: 10rem;
+		height: 2rem;
+	}
+
+	td.available {
+        background-color: darkgreen;
+    }
+
+    td.middleCapacity {
+        background-color: orange
+    }
+
+    td.capacityReached {
+        background-color: red
+    }
+
 </style>
 
 <script>
@@ -39,5 +62,28 @@
 				{/each}
 			</tr>
 		{/each}
+	</table>
+
+	<table id="legend">
+		<tr>
+			<td class="available"></td>
+			<td>Room is available</td>
+		</tr>
+		<tr>
+			<td class="middleCapacity"></td>
+			<td>Room already has reservations</td>
+		</tr>
+		<tr>
+			<td class="capacityReached"></td>
+			<td>Room has no space left</td>
+		</tr>
+		<tr>
+			<td>✔️</td>
+			<td>Book room for this day</td>
+		</tr>
+		<tr>
+			<td>❌</td>
+			<td>Cancel your reservation for this day</td>
+		</tr>
 	</table>
 </div>
